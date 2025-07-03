456df7af70f9f71b26ed1f3016caa6ea10f7efe1
Bryce Johnston <bjohnstondev@gmail.com>
2025-07-03 09:26:33 -0500

    .

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,15 @@
+import HtmlWebpackPlugin from "html-webpack-plugin";
 import baseConfig from "@stanlemon/webdev/webpack.config.js";
+import path from "path";
+
 export default {
   ...baseConfig,
   entry: "./app/index.js",
+  plugins: [
+    ...(baseConfig.plugins || []),
+    new HtmlWebpackPlugin({
+      template: path.resolve("app/index.html"),
+      filename: "index.html",
+    }),
+  ],
 };
